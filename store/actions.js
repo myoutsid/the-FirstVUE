06efa59a0fa2f9354202e3b5c1d61c46bce3bafa
cj <18137774728@163.com>
2018-11-20 21:17:47 +0800

    修改action方法

--- store/actions.js
+++ store/actions.js
@@ -50,19 +50,54 @@ export default {
 
   },
 
-
+  //保存用户信息
   saveUser ({commit},user){
     commit(RECEIVE_USER,{user})
   },
+  //获取当前用户的信息的异步action
   async getUserInfo({commit}){
     const result = await reqUserInfo()
 
     if(result.code ===0){
       commit(RECEIVE_USER,{user})
     }
-  }
+  },
 
+//  退出登陆的action
+  async logout({commit}){
+    const result = await reqLogout()
+    if (result.code === 0) {
+      commit(RESET_USER)
 
+    }
+  },
+    // 异步获取商家信息
+    async getShopInfo({commit}){
+      const result = await reqShopInfo()
+      if(result.code===0) {
+        const info = result.data
+        info.score = 3.5
+        commit(RECEIVE_INFO, {info})
+      }
+    },
+
+// 异步获取商家评价列表
+    async getShopRatings({commit}) {
+      const result = await reqShopRatings()
+      if(result.code===0) {
+        const ratings = result.data
+        commit(RECEIVE_RATINGS, {ratings})
+      }
+    },
+
+// 异步获取商家商品列表
+    async getShopGoods({commit}) {
+      const result = await reqShopGoods()
+      if(result.code===0) {
+        const goods = result.data
+        commit(RECEIVE_GOODS, {goods})
+      }
+    },
 
 
 }
